19c7b160bdbe1519f38d1be2e5462f09eb76813f
Alexander Loewi <amloewi@gmail.com>
2017-12-10 01:55:13 -0500

    Working on COW; some success, still don't have answers
    Did some new shit that might have broken it -- still don't know what to do about the e updates. Go back to optimize for the both find.x.fxns, because that was smooth (if, quickly, diverged), if nothing works -- but above all, figure out what you're SUPPOSED to be doing

--- hlsm_sim.R
+++ hlsm_sim.R
@@ -155,10 +155,10 @@ find.init <- function(m){
         
     z <- array(dim=c(N,K,2))    
     for(k in 1:K){
-        lsm.k <- ergmm(network(m[,,1]) ~ euclidean(d=2)^2)
+        lsm.k <- ergmm(network(m[,,k]) ~ euclidean(d=2)^2)
         z.k <- lsm.k$mcmc.mle$Z # pmode v. mle? # 92 EDGES? 
         a.k <- summary(lsm.k)$pmean$coef.table[1]$Estimate # jesus christ, latentnet
-        # No not quite!
+        # Not quite done yet!
         z.k <- procrustean(z.k, z.avg) 
         z[,k,] <- z.k
     }
@@ -206,7 +206,7 @@ library(latentnet)
 
 
 # Designed to go from parameters to model in one line
-one.shot <- function(edges, sigma, file, model_name, iter){
+one.shot <- function(edges, init, sigma, iter, file, model_name){
     
     N <- dim(edges)[1]
     K <- dim(edges)[3]
@@ -227,16 +227,16 @@ one.shot <- function(edges, sigma, file, model_name, iter){
                       sigma_b=sigma*diag(2), # [s   0; 0   s] (one matrix)
                       sigma_z=sigmat)        # [s_k 0; 0 s_k] (one matrix per layer)
     
-    hlsm.init <- find.init(edges)
+    #hlsm.init <- find.init(edges)
     fit <- stan(file=file,
                 model_name=model_name,
                 iter=iter,
                 data=hlsm.data,
-                init=hlsm.init,
+                init=init, #hlsm.init,
                 verbose=T,
                 control=list(max_treedepth = 15))
     theta <- unpack.hlsm.fit(fit, N, K)#, which=which)
-    return(list(fit=fit, theta=theta, init=hlsm.init()))
+    return(list(fit=fit, theta=theta, init=init()))
 }
 
 
@@ -333,9 +333,9 @@ plot.model <- function(m, alpha=NULL, which="max", xlim=NULL, ylim=NULL){
     init <- m$init$b
     
     if(is.null(alpha))
-        alpha <- m$theta$alpha
+        alpha <- theta$alpha
     plot.positions(z, b, alpha=alpha, xlim=xlim, ylim=ylim)
-    plot.lsm(alpha=5, z=init, col="blue", add=T)
+    plot.lsm(alpha=1, z=init, col="blue", add=T)
 }
 
 ####### FUNCTION TESTS #######
@@ -435,10 +435,203 @@ ovals.init <- find.init(two.ovals)
 model.accuracy(two.ovals, ovals.init()$z, ovals.init()$alpha) # Before: not BAD, 
 model.accuracy(two.ovals, two.ovals.model$theta$z, two.ovals.model$theta$alpha) # WORSE
 
-two.ovals.model <- one.shot(two.ovals, sigma, "hlsm.stan", "two_ovals", 8e4)
-traceplot(two.ovals.model$fit)
+# SOME TESTS WITH -- REDUCING SIGMA. Issue, though, is -- how many iterations?
+ovals.init <- find.init(two.ovals) # done outside so you can plot them
+plot.positions(ovals.init()$z, ovals.init()$b, ovals.init()$alpha)
+two.ovals.model <- one.shot(two.ovals, ovals.init, .1, 2e4, # 4e3 not enough ... 2e4?
+                            "hlsm.stan", "two_ovals")
+traceplot(two.ovals.model$fit) # I've never seen the z/e plots -- 
+# Also, gotta do better than the ... 
 plot.model(two.ovals.model, which="max") # the best
+# WHY ARE THEY ALL ON THAT FUCKIN DIAGONAL? Am I even DOING this right?
 plot.errors(two.ovals.model$theta$z, two.ovals.model$theta$b)
+
+# Hm ... LOOKS good, but the errors are shitty. Hm ... procrustes might be the wrong
+# transpose. I don't want to match the shape, I want to match the node positions --
+# is it doing that? How do I minimize the distances between THEM?
+
+# PLOT THE INIT POSITIONS, TOO
+# MAKE MOVES (coord-wise, x/y)
+# VISUAL MOVES
+
+my.transform <- function(){
+    # Rotate, until you min distances between original points
+    # translate, for same
+    # Is this the same as procrustes? Should look it up --
+    # ALSO -- coordinate descent/convexity for LSMs in general
+}
+
+
+# uniroot! What a great name!
+
+sigma <- function(x) 1/(1+exp(-x))
+L22 <- function(x) sum(x^2) 
+
+# For speed, I should check -- this against other things
+build.b.fxn <- function(y, a, b, e, i, axis){
+    f <- function(x){
+        total <- 0
+        for(j in (1:N)[-i]){ # Does this even matter? As long as the intercept's consistent
+            for(k in 1:K){
+                bi <- b[i,]
+                bi[axis] <- x
+                
+                d <- bi + e[i,k,] - b[j,] - e[j,k,]
+                r <- sigma(a - L22(d)) - y[i,j,k] # This should be a scalar
+                
+                total <- total + r*sqrt(L22(d)) # right, THIS. Fuck. Kay ... d is the problem
+                
+            }
+        }
+        return(-total)
+    }
+    return(f)
+}
+
+build.e.fxn <- function(y, a, b, e, i, k, axis, l){
+    f <- function(x){
+        total = 0
+        for(j in (1:N)[-i]){ # Does this even matter? As long as the intercept's consistent
+            eik <- e[i,k,]
+            eik[axis] <- x
+            d <- b[i,] + eik - b[j,] - e[j,k,]
+            r <- sigma(a - L22(d)) - y[i,j,k]
+            total <- total + r*sqrt(L22(d)) # + -l/l #? HOW DO I SOLVE THESE? manually? line search?
+        }
+        return(-total)
+    }
+    return(f)
+}
+
+lkhd <- function(y, a, b, e, l){
+    total <- 0
+    N <- dim(y)[1]
+    K <- dim(y)[3]
+    for(k in 1:K){
+        for(i in 1:N){
+            for(j in 1:N){
+                s <- sigma(a[k] - L22(b[i,] + e[i,k,] - b[j,] - e[j,k,]))
+                y. <- y[i,j,k]    
+                total <- total + s^y.*(1-s)^(1-y.)
+            }
+        }
+    }
+    total <- total - l*sum(abs(e)) # right -- subtract, for max (REMEMBER IT'S MAX)
+}
+
+coord.opt <- function(y, a, b, e, l, maxit=1){
+
+    iter <- 1
+    lkhd <- array(dim=maxit)
+    while(iter < maxit){ # Also a convergence criterion -- what? 
+        # For each layer, first update the layer, then the base
+        for(k in 1:K){ 
+            # update the e_ik
+            for(i in 1:N){
+                for(axis in 1:2){
+                    f.bi <- build.b.fxn(y, a[k], b, e, i, axis)
+                    #bi.star <- uniroot(f.bi, c(0,.), extendInt = "yes")$root
+                    bi.star <- optimize(f.bi, c(-20,20))$minimum
+                    b[i,axis] <- bi.star
+                }
+            }
+            # update the b_i
+            for(i in 1:N){
+                for(axis in 1:2){
+                    f.eik <- build.e.fxn(y, a[k], b, e, i, k, l, axis)
+                    # eik.star <- uniroot(f.eik, c(9,10), extendInt = "downX")$root
+                    # eik.star <- optimize(f.eik, c(-20,20))$minimum
+                    
+                    obj.min <- Inf
+                    eik.star <- NULL
+                    for(v in seq(-20,20,length=1000)){
+                        obj.val <- f.eik(v)
+                        if(obj.val < obj.min){
+                            obj.min <- obj.val
+                            eik.star <- v
+                        }
+                        if(obj.val > -l & obj.val < l){ # if v \in [-lambda, lambda]
+                            eik.star <- 0
+                            break
+                        }
+                    }
+                    e[i,k,axis] <- eik.star
+                    
+                }
+            }
+        }
+        lkhd[iter] <- lkhd(y, a, b, e, l)
+        iter <- iter + 1
+    }
+    theta <- list(a=a, b=b, e=e, l=l, lkhd=lkhd)
+    return(theta)
+}
+
+
+# THERE IS no 'e' variable right now -- e = z - b, but the indices are a little tricky
+a  <- ovals.init()$alpha
+b. <- ovals.init()$b # CAREFUL -- don't overwrite the REAL b
+z  <- ovals.init()$z
+e  <- array(dim=c(N,K,2))
+for(k in 1:K){
+    e[,k,] <- z[,k,] - b.
+}
+lambda <- 1
+f.bi  <- build.b.fxn(two.ovals, 1, b, e, 1, 1)
+f.eik <- build.e.fxn(two.ovals, 1, b, e, 1, 1, 1, 1) # fuck, can be maximixed (~11)
+
+out <- optimize(f.bi, c(0,20)) # except THAT was ok -- hm, got the edge though -- 
+
+plot(sapply(-10:10, f.bi)) # hm -- negative forever? Oh also, MAX OR MIN?
+# ... isn't what what I'm trying do -- do what with, now?
+ur <- uniroot(f.bi, c(-3,0), extendInt = "yes") # WHAT THE FUCK, UNIROOT
+ur$root
+
+
+abline(h=0)
+out$minimum # what ... .47 ? oh it does it by INDEX .. wait now ... oh btwn 1/2. Yes!
+
+f.eik(-1) # ah, makes a VECTOR -- hm. f.bi(0) # ah, makes a VECTOR -- hm. 
+
+source("/path/to/file/my_fn_lib1.r")
+
+
+# I should really check -- all sorts of things. lkhd, accuracy, effectiveness
+# of the individual optimizations ... BUT, it looks like it's RUNNING. At least.
+
+# with lambda=1, everything got zeroed out. Dropping to .1.
+out <- coord.opt(two.ovals, a, b., e, .1, maxit=20) # FUCK. What is it and why? 'a'.
+
+# Okay well now, it's all OVER the fucking place. K -- fuck. Need to back up,
+# commit, and try again. Fuck.
+plot(out$lkhd) # IT IS INCREASING. LET'S SEE HOW LONG. (Does it plateau?)
+
+# Hm -- peaked soon, then -- dropped precipitously. Hm. K. What was happening?
+# WHAT'S HAPPENING WITH ACCURACY?
+plot.positions(ovals.init()$z, ovals.init()$b, ovals.init()$alpha)
+z.out  <- array(dim=c(N,K,2))
+for(k in 1:K){
+    z.out[,k,] <- out$b + out$e[,k,]
+}
+plot.positions(z.out, out$b, .4) # why is alpha still a fucking problem?
+plot.errors(z.out, out$b) 
+
+
+
+
+
+
+
+
+
+
+# Okay so that doesn't work -- since it's 1d, do some plots of the f.bi, f.eik etc.,
+# see if they HAVE roots (among other things)
+
+
+
+
+
 
 
 
